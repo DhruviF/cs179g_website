1d605cf06ae42ea258861fd86c521383b45ca79c
Jerry <xuveda@gmail.com>
2022-11-30 11:35:51 -0800

    added backendstuff + changes that be discarded

---
+++ src/dbFiles/dbOperations.js
@@ -0,0 +1,40 @@
+const config    = require('./dbConfig')
+const sql = require('mssql')
+
+const getCancelledAmount = async() =>{
+    try{
+        let pool = await sql.connect(config);
+        let cancelled = pool.request().query("SELECT * FROM allcancelledFlights")
+
+        console.log(cancelled)
+        return cancelled
+    }
+    catch(error){
+        console.log(error)
+    }
+
+}
+
+const searchBarQuery = async(airline, destination, origin, month, day, year) =>{
+    try{
+        let pool = await sql.connect(config);
+        let rows = pool.request().query(`  SELECT TOP(10)* 
+                                                FROM searchBar 
+                                                WHERE   airline = '${airline}' 
+                                                    AND destCode = '${destination}' 
+                                                    AND originCode = '${origin}'
+                                                    AND month = ${month}
+                                                    AND dayofMonth = ${day}
+                                                    AND year = ${year}`)
+        console.log(rows)
+        return rows
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+module.exports = {
+    searchBarQuery,
+    getCancelledAmount
+}
